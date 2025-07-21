a1a601dcbb55aa11b48109d702950931174b4b3c
morteza <nancologist@gmail.com>
2025-07-21 17:17:31 +0200

    Init

---
+++ src/test/java/com/haiilo/kata/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiilo.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
